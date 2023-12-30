f55bb090227a7f9cf93307216e8dc462a3df2d72
Oliver Mazariegos <oliver.mazariegos98@gmail.com>
2023-12-30 17:58:15 -0600

    feat: initial setup, download data

---
+++ run.R
@@ -0,0 +1,6 @@
+Sys.setlocale(locale = "es_ES.UTF-8")
+# SETUP                             -------------------------------
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+rm(list = ls())
+
+source("setup.R")
