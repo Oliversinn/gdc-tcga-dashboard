38d8483fa25450eb426fe75be33198e558de83da
Oliver Mazariegos <oliver.mazariegos98@gmail.com>
2023-12-30 17:59:06 -0600

    feat: setup

--- setup.R
+++ setup.R
@@ -1,47 +1,116 @@
 library(GenomicDataCommons)
+library(dplyr)
 
+# setup ----
+path_global = dirname(rstudioapi::getSourceEditorContext()$path)
 
-projects <- GenomicDataCommons::projects() %>%
-  GenomicDataCommons::filter(program.name == "TCGA") %>%
-  GenomicDataCommons::facet(c("name", "project_id")) %>%
-  results_all()
+# Get diagnosis, demographic and project ----
+expands = c("diagnoses", "demographic", "project")
+clin_results = cases() %>%
+  GenomicDataCommons::select(
+    c(
+      "disease_type",
+      "primary_site"
+    )
+  ) %>% 
+  GenomicDataCommons::filter(project.program.name == "TCGA") %>%
+  GenomicDataCommons::expand(expands) %>%
+  GenomicDataCommons::results_all()
 
-tcga_project_ids_list <- projects$project_id
+# combined_cases ----
+combined_cases <- data.frame(
+  case_id = clin_results$case_id,
+  project_id = clin_results$project$project_id,
+  project_name = clin_results$project$name
+) %>% 
+  left_join(
+    .,
+    data.frame(
+      case_id = clin_results$id,
+      disease_type = clin_results$disease_type,
+      primary_site = clin_results$primary_site
+    ),
+    join_by(case_id)
+  )
 
-projects_info <- projects() %>%
-  GenomicDataCommons::filter(project_id %in% tcga_project_ids) %>%
-  results_all()
+# combined_diagnoses ----
+combined_diagnoses <- lapply(
+  clin_results$diagnoses, function(inner_list) inner_list
+) %>%
+  bind_rows(.id = "case_id") %>% 
+  select(
+    where(
+      ~!all(is.na(.x))
+    )
+  )
 
-diseases_types <- unique(
-  rapply(projects$disease_type, function(x) head(x, 100))
-)
+# combined_demographics ----
+combined_demographics <- as.data.frame(clin_results$demographic) %>% 
+  select(
+    where(
+      ~!all(is.na(.x))
+    )
+  )
 
-primary_sites <- unique(
-  rapply(projects$primary_site, function(x) head(x, 100))
-)
-
-cases_pre_info <- cases() %>%
-  GenomicDataCommons::filter(project.project_id %in% tcga_project_ids_list) %>%
-  results_all()
-
-cases_info <- GenomicDataCommons::cases() %>%
-  GenomicDataCommons::filter(project.project_id %in% tcga_project_ids_list) %>%
-  GenomicDataCommons::facet(c(
-    "project.project_id",
-    "demographic.race",
-    "demographic.gender",
-    "demographic.ethnicity",
-    "demographic.vital_status",
-    "diagnoses.primary_diagnosis",
-    "diagnoses.site_of_resection_or_biopsy",
-    "primary_site"
-  )) %>%
-  aggregations()
-
-
+# files_info ----
 files_info <- GenomicDataCommons::files() %>%
-  GenomicDataCommons::filter(access == "open") %>%
+  GenomicDataCommons::filter(
+    cases.project.program.name == "TCGA"
+  ) %>%
+  GenomicDataCommons::filter(
+    access == "open"
+  ) %>% 
   GenomicDataCommons::facet(
-    c("type", "data_type", "analysis.workflow_type")
+    c(
+      "data_category",
+      "data_type",
+      "data_format",
+      "type",
+      "file_id",
+      "experimental_trategy",
+      "wgs_coverage"
+    )
   ) %>%
   aggregations()
+
+# files_result ----
+files_results <- GenomicDataCommons::files() %>% 
+  GenomicDataCommons::filter(
+    cases.project.program.name == "TCGA"
+  ) %>%
+  GenomicDataCommons::filter(
+    access == "open"
+  ) %>% 
+  GenomicDataCommons::select(
+    c(
+      "file_id",
+      "cases.project.project_id",
+      "cases.case_id",
+      "data_category",
+      "data_type",
+      "data_format",
+      "experimental_strategy",
+      "wgs_coverage"
+    )
+  ) %>% 
+  GenomicDataCommons::results_all()
+
+# files_cases ----
+files_cases <- lapply(
+  files_results$cases, function(inner_list) inner_list
+) %>%
+  bind_rows(.id = "file_id") %>% 
+  select(
+    where(
+      ~!all(is.na(.x))
+    )
+  ) %>% 
+  unnest(., project)
+
+files_results <- as.data.frame(
+  within(files_results, rm(cases))
+)
+
+tcga_project_ids_list <- unique(combined_cases$project_id)
+
+save.image(file = paste0(path_global, "/Dashboard/tcga.RData"))
